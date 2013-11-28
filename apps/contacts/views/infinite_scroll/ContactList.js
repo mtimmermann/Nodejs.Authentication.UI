81034e338fc3516a4224788d2b04ea5fa0dec1ee
Mark Timmermann <mtimmermann@hotmail.com>
2013-11-28 12:43:31 -0700

    Infinite scroll, scroll listener. WIP

--- apps/contacts/views/infinite_scroll/ContactList.js
+++ apps/contacts/views/infinite_scroll/ContactList.js
@@ -12,8 +12,11 @@ define(function(require, exports, module) {
         itemView: ContactListItemView,
         //collection: Contacts,
 
+        // http://backbonetutorials.com/infinite-scrolling/
+
         events: {
-            'click [data-inf-scroll-add="button"]': 'getNext'
+            'click [data-inf-scroll-add="button"]': '_getNext'
+            //'scroll': 'checkScroll'
         },
 
         initialize: function(options) {
@@ -21,13 +24,41 @@ define(function(require, exports, module) {
             if (!options.collection) {
                 throw (new Error('ContactList View: collection option is required'));
             }
+
+            this._isLoading = false; // True if currently requesting a page
+
+            var self = this;
+            this._scrollListener = function(evt) {
+                self._checkScroll();
+            };
+            window.addEventListener('scroll', this._scrollListener, false);
+
         },
 
-        getNext: function() {
+        _checkScroll: function() {
+            console.log('checkScroll');
+            if (this.collection.currentPage === this.collection.totalPages) {
+                // Remove the scroll listener
+                window.removeEventListener('scroll', this._scrollListener, false);
+                return; // Disabled
+            }
+
+            //var triggerPoint = 100; // 100px from the bottom
+            var triggerPoint = 20;
+
+            //http://stackoverflow.com/questions/3898130/how-to-check-if-a-user-has-scrolled-to-the-bottom
+            if (!this._isLoading &&
+                ($(window).scrollTop() + $(window).height() > $(document).height() - triggerPoint)) {
+                    this._getNext();
+            }
+        },
+
+        _getNext: function() {
             var self = this;
 
             // Save the current collection list perform page.
             var currentList = _(this.collection.models).clone();
+            this._isLoading = true;
             this.collection.nextPage({});
             this.collection.getCollection();
 
@@ -38,6 +69,7 @@ define(function(require, exports, module) {
                     currentList.push(model);
                 });
                 self.collection.reset(currentList);
+                self._isLoading = false;
             });
         }
 
